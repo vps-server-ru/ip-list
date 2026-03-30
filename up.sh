9b492cbd3a4b3e7b22cd68d3911bbc84fb081c81
vps-server-ru <support@vps-server.ru>
2026-03-30 17:49:38 +0300

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# set here tun iface
+TUN=tun0
+# MAX.RU -> blackhole
+ip route add blackhole 155.212.204.0/24
+# ip-list
+wget -qO - http://openwrt.ivtrans.ru/ip-list.txt > /tmp/ip-list.txt
+while read line; do
+  ip r a $line dev $TUN
+done < /tmp/ip-list.txt
+# google IPs
+wget -qO - https://www.gstatic.com/ipranges/goog.json |grep ipv4Prefix|awk '{print $2}' |sed 's/"//g' > /tmp/google.txt
+while read line; do
+  # echo $line
+  ip r a $line dev $TUN
+done < /tmp/google.txt
+date > /tmp/$TUN-up.txt
